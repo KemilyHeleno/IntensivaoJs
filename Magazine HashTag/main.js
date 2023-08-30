a75b145f3114fcd67942f03672eba80d684e5060
Kemily <kemily.r.heleno@gmail.com>
2023-08-29 22:42:22 -0300

    atualizacoes

--- Magazine HashTag/main.js
+++ Magazine HashTag/main.js
@@ -6,33 +6,60 @@
 const catalogo = [
   {
     id: 1,
-  nomeProduto: "Vestido Preto",
-  marcaProduto: "Shein",
-  preco: 111.9,
-  nomeArquivoImagem: "1.png",
-},
-{
+    nomeProduto: "Vestido Preto",
+    marcaProduto: "Shein",
+    preco: 111.9,
+    nomeArquivoImagem: "1.png",
+  },
+  {
     id: 2,
     nomeProduto: "Vestido Verde Escuro",
     marcaProduto: "Shein",
     preco: 150.0,
     nomeArquivoImagem: "2.png",
+  },
+  {
+    id: 3,
+    nomeProduto: "Vestido Verde Escuro",
+    marcaProduto: "Shein",
+    preco: 150.0,
+    nomeArquivoImagem: "3.png",
+  },
+  {
+    id: 4,
+    nomeProduto: "Vestido Verde Escuro",
+    marcaProduto: "Shein",
+    preco: 150.0,
+    nomeArquivoImagem: "4.png",
+  },
+  {
+    id: 5,
+    nomeProduto: "Vestido Verde Escuro",
+    marcaProduto: "Shein",
+    preco: 150.0,
+    nomeArquivoImagem: "5.png",
+  },
+  {
+    id: 6,
+    nomeProduto: "Vestido Verde Escuro",
+    marcaProduto: "Shein",
+    preco: 150.0,
+    nomeArquivoImagem: "6.png",
   }
 ];
 
-for (const produtoCatalogo of catalogo){
-    const cartaoProduto = `<div id="card-produto-1"> <!-- divisão entre produtos -->
+for (const produtoCatalogo of catalogo) {
+  const cartaoProduto = `<div id="card-produto-1"> <!-- divisão entre produtos -->
     <img 
         src="./assets/images/${produtoCatalogo.nomeArquivoImagem}" 
-        alt="Vestido preto com manga curta bufante e cintura alinhada." 
+        alt="Produto." 
         style="height: 200px;"
     />
-    <p>${produtoCatalogo.marcaProduto}</p>
+    <p class='nomeMarca'>${produtoCatalogo.marcaProduto}</p>
     <p>${produtoCatalogo.nomeProduto}</p>
     <p>R$ ${produtoCatalogo.preco}</p>
     <button>Adicionar</button>
     </div>`;
-    
-    document.getElementById("container-produto").innerHTML += cartaoProduto; // Recupere um elemento a partir do id dele
-    
+
+  document.getElementById("container-produto").innerHTML += cartaoProduto; // Recupere um elemento a partir do id dele
 }
--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Intensivão JavaScript da HashTag Treinamentos
 ## Códigos
 Códigos das aulas para adquirir o certificado do curso
 * Aula 01 - 28/08/2023 - **JS2023**
+
+## Links importantes
+
+[vite] (https://vitejs.dev/) - site usado como servidor local para o site rodar enquanto é programado.
+
