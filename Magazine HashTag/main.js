7981fab615d45a6f0ecb9924ece81f96fd0ed8bc
Kemily <kemily.r.heleno@gmail.com>
2023-08-30 01:54:23 -0300

    aula 2 do intensivão de javascript

--- Magazine HashTag/main.js
+++ Magazine HashTag/main.js
@@ -1,8 +1,4 @@
-// const nomeProduto = "Vestido Preto";
-// const marcaProduto = "Zara";
-// const precoProduto = 111.90;
-// const nomeArquivoImagem = "1.png";
-
+import { inicializarCarrinho } from "./src/menuCarrinho";
 const catalogo = [
   {
     id: 1,
@@ -20,7 +16,7 @@ const catalogo = [
   },
   {
     id: 3,
-    nomeProduto: "Vestido Verde Escuro",
+    nomeProduto: "Regata Justinha Branca",
     marcaProduto: "Shein",
     preco: 150.0,
     nomeArquivoImagem: "3.png",
@@ -45,21 +41,23 @@ const catalogo = [
     marcaProduto: "Shein",
     preco: 150.0,
     nomeArquivoImagem: "6.png",
-  }
+  },
 ];
-
+// console.log("funcionei");
 for (const produtoCatalogo of catalogo) {
-  const cartaoProduto = `<div id="card-produto-1"> <!-- divisão entre produtos -->
+  const cartaoProduto = `<div class='border-solid border-2 border-sky-500 w-48 m-2' id="card-produto-${produtoCatalogo.id}">
     <img 
         src="./assets/images/${produtoCatalogo.nomeArquivoImagem}" 
         alt="Produto." 
-        style="height: 200px;"
+        style="height: 255px;"
     />
     <p class='nomeMarca'>${produtoCatalogo.marcaProduto}</p>
     <p>${produtoCatalogo.nomeProduto}</p>
     <p>R$ ${produtoCatalogo.preco}</p>
     <button>Adicionar</button>
     </div>`;
-
-  document.getElementById("container-produto").innerHTML += cartaoProduto; // Recupere um elemento a partir do id dele
+  document.getElementById("container-produto").innerHTML += cartaoProduto;
 }
+// console.log("passei");
+
+inicializarCarrinho();
--- README.md
+++ README.md
@@ -4,8 +4,22 @@ Intensivão JavaScript da HashTag Treinamentos
 ## Códigos
 Códigos das aulas para adquirir o certificado do curso
 * Aula 01 - 28/08/2023 - **JS2023**
+* Aula 02 - 29/08/2023 - **CSS2908**
+
+## Anotações
+* Sempre que possível usar o display: flex para o desenvolvimento de sites, pois assim o desing do site consegue se adaptar ao tamanho da tela em que o site está sendo acessado.
+* - De preferência usar o `flex flex-wrap` pois assim os itens que não couberem na mesma linha serão jogados para a linha de baixo, diminuindo o risco de distorção de imagens por  falta de espaço.
+
+* getElementById = Recupera um elemento a partir do id dele
+
+* addEventListener = Faz o elemento escutar um evento, ele vai aguardar determinado evento ocorrer para realizar uma ação
 
 ## Links importantes
 
-[vite](https://vitejs.dev/) - site usado como servidor local para o site rodar enquanto é programado.
+[Vite](https://vitejs.dev/) - site usado como servidor local para o site rodar enquanto é programado.
 
+[Tailwind css](https://tailwindcss.com/) - estilos prontos do css para incorporar um desing legal aos sites
+
+[Baixar Font awesome](https://cdnjs.com/libraries/font-awesome) - Baixar informações necessárias para que o font awesome funcione na aplicação
+
+[Font awesome](https://fontawesome.com/) - biblioteca de ícones
